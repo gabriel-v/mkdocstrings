35c9456f4ffa76b273faa7ae93a04351e23fafea
Timothée Mazzucotelli <pawamoy@pm.me>
2021-03-17 20:43:03 +0100

    fixup! feat: Generate objects inventory

--- src/mkdocstrings/inventory.py
+++ src/mkdocstrings/inventory.py
@@ -1,17 +1,22 @@
-import sphobjinv as soi
+import zlib
 
 
-def get_inventory(autorefs, project="project", version="0.0.0"):
-    inv = soi.Inventory()
-    inv.project = project
-    inv.version = version
+HEADER = """# Sphinx inventory version 2
+# Project: {project}
+# Version: {version}
+# The remainder of this file is compressed using zlib.
+"""
 
+
+def get_inventory(autorefs, project="project", version="0.0.0", compress=False):
+    header = HEADER.format(project=project, version=version).encode("utf8")
+
+    lines = []
     for anchor, url in autorefs._url_map.items():
         url = url.replace(anchor, "$")
-        o = soi.DataObjStr(name=anchor, domain="py", uri=url, role="obj", priority="1", dispname="-")
-        inv.objects.append(o)
+        lines.append(f"{anchor} py:obj 1 {url} -".encode("utf8"))
 
-    df = inv.data_file()
-    return df
-    #return soi.compress(df)
-    #soi.writebytes("objects_attrs_new.inv", dfc) 
+    data = b"\n".join(lines)
+    if compress:
+        data = zlib.compress(data, 9)
+    return header + data
